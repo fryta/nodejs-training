950bfcd4ea06984dbbaf994e7db71f9fd96cc137
Wojciech Frycz <wojciech.frycz@grandparade.co.uk>
2019-09-30 15:01:56 +0200

    Next step of refactoring - bookController

--- node_books/src/bookRoutes.js
+++ node_books/src/bookRoutes.js
@@ -1,39 +1,7 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const controller = require("./bookController");
 
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017/booksapi';
-
-let booksPromise = MongoClient.connect(url, {bufferMaxEntries: 0}).then(function(client){
-  return client.db().collection("books");
-});
-
-router.post("/book", async function(req, res, next) {
-  try{
-    const {title, authors, isbn, description} = req.body;
-    const books = await booksPromise;
-
-    await books.updateOne(
-      {isbn: isbn},
-      { $set: {title, authors, isbn, description} },
-      {upsert: true}
-    );
-
-    return res.json({title, authors, isbn, description});
-  } catch (e) {
-    next(e);
-  }
-});
-
-router.get("/book/:isbn", async function (req, res, next) {
-  try{
-    const isbn = req.params.isbn;
-    const books = await booksPromise;
-    const book = await books.findOne({isbn}, {projection: {_id: false}});
-
-    return res.json(book);
-  } catch (e) {
-    next(e);
-  }
-});
+router.post("/book", controller.createOrUpdate);
+router.get("/book/:isbn", controller.details);
 
 module.exports = router;
