6ad731144540861a42e76b79e708238bc2fcaff2
Wojciech Frycz <wojciech.frycz@grandparade.co.uk>
2019-10-01 10:27:56 +0200

    Removed duplication in bookController

--- node_books/src/bookController.js
+++ node_books/src/bookController.js
@@ -1,35 +1,43 @@
-module.exports = ({bookRepository, bookService}) => ({
-  async createOrUpdate(req, res, next){
-    try{
-      const book = req.body;
+const mapValues = require("lodash.mapvalues");
 
-      await bookService.createOrUpdate(book);
+const wrapWithTryCatch1 = fn =>
+  (req, res, next) =>
+    Promise.resolve(fn(req, res, next)).catch(next);
 
-      return res.redirect(`/book/${book.isbn}`);
-    } catch (e) {
-      next(e);
-    }
-  },
-  async details(req, res, next){
-    try{
-      const isbn = req.params.isbn;
-
-      const book = await bookRepository.find({isbn});
-
-      return book ? res.json(book) : next();
-    } catch (e) {
-      next(e);
-    }
-  },
-  async remove(req, res, next){
-    try{
-      const isbn = req.params.isbn;
-
-      await bookRepository.remove({isbn});
-
-      return res.status(204).end();
+function wrapWithTryCatch(fn) {
+  return async function (req, res, next) {
+    try {
+      await fn(req, res, next)
     } catch (e) {
       next(e);
     }
   }
-});
+}
+
+function withErrorHandling(api) {
+  return mapValues(api, wrapWithTryCatch);
+}
+
+module.exports = ({bookRepository, bookService}) => (withErrorHandling({
+  async createOrUpdate(req, res, next){
+    const book = req.body;
+
+    await bookService.createOrUpdate(book);
+
+    return res.redirect(`/book/${book.isbn}`);
+  },
+  async details(req, res, next){
+    const isbn = req.params.isbn;
+
+    const book = await bookRepository.find({isbn});
+
+    return book ? res.json(book) : next();
+  },
+  async remove(req, res, next){
+    const isbn = req.params.isbn;
+
+    await bookRepository.remove({isbn});
+
+    return res.status(204).end();
+  }
+}));
