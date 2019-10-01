806c9b7fe01b41254e723d2a577599653fdb64a8
Wojciech Frycz <wojciech.frycz@grandparade.co.uk>
2019-10-01 16:04:44 +0200

    MongoDB repository layer

---
+++ events/src/es.js
@@ -0,0 +1,26 @@
+function initStore() {
+  const es = require('eventstore')({
+    type: 'mongodb',
+    host: 'localhost',
+    port: 27017,
+    dbName: 'card_eventstore',
+    eventsCollectionName: 'events',
+    snapshotsCollectionName: 'snapshots',
+    transactionsCollectionName: 'transactions',
+    timeout: 10000,
+    options: {
+      useNewUrlParser: true
+    }
+  });
+  return new Promise(function(resolve, reject) {
+    es.init(function (err) {
+      if(err) reject(err);
+
+      es.close = es.store.db.close;
+
+      resolve(es);
+    });
+  });
+}
+
+module.exports = initStore;
