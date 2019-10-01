a2c9161114763067c7064eba65062ae8870852fe
Wojciech Frycz <wojciech.frycz@grandparade.co.uk>
2019-10-01 13:44:58 +0200

    Tests passing for card

--- events/src/card.js
+++ events/src/card.js
@@ -1,16 +1,35 @@
 module.exports = function card() {
+  let limit = null;
+  let cash = 0;
+
+  const limitAssigned = () => limit !== null;
+  const notEnoughMoney = (amount) => cash < amount;
+
   return {
     assignLimit(amount) {
+      if (limitAssigned()) {
+        throw new Error("Cannot assign limit for the second time");
+      }
 
+      limit = amount;
+      cash = amount;
     },
     availableLimit() {
-
+      return cash;
     },
     withdraw(amount) {
+      if (!limitAssigned()) {
+        throw new Error("No limit assigned");
+      }
 
+      if(notEnoughMoney(amount)){
+        throw new Error("Not enough money");
+      }
+
+      cash -= amount;
     },
     repay(amount) {
-
+      cash += amount;
     }
   };
 };
